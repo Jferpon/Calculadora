b6a1c9356be4dcc5679eb9e2acb667f39da6270b
Jferpon <jferpon089@g.educaand.es>
2025-05-16 10:42:01 +0200

    Implementacion codigo main

---
+++ Main.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+    	JFrame frame = new JFrame("Calculadora");
+    	frame.setBounds(250, 250, 0, 0);
+    	frame.add(new Panel());
+   	 
+    	frame.setVisible(true);
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	frame.pack();
+   	 
+	}
+    
+}
